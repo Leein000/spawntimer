478733b8c517b949aedb322f6cc6966350e730d2
user <Leein05.07.28@gmail.com>
2021-12-29 07:48:58 +0900

    데스타이머 수정

--- src/main/java/respawn/spawn/Spawn.java
+++ src/main/java/respawn/spawn/Spawn.java
@@ -9,6 +9,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+
 import java.util.HashMap;
 import java.util.UUID;
 
